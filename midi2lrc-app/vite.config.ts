4aabd78f5e8844081a9ea8b9a381a4a41ed2f790
simonstasius <simon@thecoding.de>
2026-06-09 13:46:04 +0200

    Fix script source path in index.html and update Vite config for build output directory

--- midi2lrc-app/vite.config.ts
+++ midi2lrc-app/vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Midi2LRC/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+  },
 })
